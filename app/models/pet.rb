296ed2092ef79be1de88c37c242f2b620e29ed5c
germain loret <germain.loret@gmail.com>
2020-08-11 18:03:17 +0200

    Implement CRUD actions for Pet model

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,5 @@
 class Pet < ApplicationRecord
+  SPECIES = ["Dog", "Cat", "Bird", "Snake", "Horse"]
   validates :name, presence: true
-  validates :species, inclusion: { in: ["Dog", "Cat", "Bird"], message: "%{value} is not included in the list" }
+  validates :species, inclusion: { in: SPECIES, message: "%{value} is not included in the list" }
 end
