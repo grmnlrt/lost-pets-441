ccb40344df980c0b330f74a0619009b1911a6f29
germain loret <germain.loret@gmail.com>
2020-08-11 17:22:42 +0200

    Add validations to Pet model

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,2 +1,4 @@
 class Pet < ApplicationRecord
+  validates :name, presence: true
+  validates :species, inclusion: { in: ["Dog", "Cat", "Bird"], message: "%{value} is not included in the list" }
 end
